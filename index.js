d347741671fbfd43a9b40b4824ffdf6cc0e59776
Joyce Liu <liujoycec@gmail.com>
2015-08-10 21:33:28 -0700

    Set default values for origin, target, and nt so that solve only needs to take in one argument.

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ var move = actions.move.bind(actions);
 
 
 function solve (n, origin, target, nt) {
-  console.log('n: '+n+' origin: '+origin+" target: "+target);
+	// sets default positions
+  if (arguments.length) {
+    origin = 0;
+    target = 2;
+    nt = 1;
+  }
+  // Base case is implied: if n = 0, then do nothing
+  // Recursive case:
   if (n > 0) {
    // var nt = notTarg(origin, target);
     solve(n-1, origin, nt, target);
@@ -14,5 +21,5 @@ function solve (n, origin, target, nt) {
   }
 }
 
-solve(config.pieces, 0, 2, 1); //n === 3
+solve(config.pieces);
 actions.finish();
