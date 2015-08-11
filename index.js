ff2c5b1f2ebf85c32a78ff4b71a0647fcfbb8630
Joyce Liu <liujoycec@gmail.com>
2015-08-10 21:26:58 -0700

    Solved problem

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var config = require("./config");
 
 var move = actions.move.bind(actions);
 
-function solve(n){
-  // fill in solution here
 
-
-
-
-
+function solve (n, origin, target, nt) {
+  console.log('n: '+n+' origin: '+origin+" target: "+target);
+  if (n > 0) {
+   // var nt = notTarg(origin, target);
+    solve(n-1, origin, nt, target);
+    move(origin, target);
+    solve(n-1, nt, target, origin);
+  }
 }
 
-solve(config.pieces);
+solve(config.pieces, 0, 2, 1); //n === 3
 actions.finish();
